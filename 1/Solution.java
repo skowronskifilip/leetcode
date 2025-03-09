2fa8267f9d22340c116da1471a2683bb54d69c0e
Filip <39197656+skowronskifilip@users.noreply.github.com>
2025-03-09 12:02:33 +0100

    solved problem #1

---
+++ 1/Solution.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int x = target - nums[i];
+            if (hashMap.containsValue(x)) {
+                return new int[]{hashMap.get(x), i};
+            }
+            hashMap.put(x, i);
+        }
+        return new int[]{};
+    }
+}
